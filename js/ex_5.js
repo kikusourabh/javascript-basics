660d1053b0202349d32842d66cb6a40d863adc59
kikusourabh <karmakarsourabh7@gmail.com>
2020-08-11 17:34:37 +0530

    external javascript file example

---
+++ js/ex_5.js
@@ -0,0 +1,6 @@
+function changeImageToSmile() {
+    document.getElementById('myimg').src = 'asset/smile-emoji.png'
+}
+function changeImageToSad() {
+    document.getElementById('myimg').src = 'asset/sad-emoji.jpg'
+}
